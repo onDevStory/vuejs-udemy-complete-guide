6c040b9ed76b9bad94f9ff5a4bea6d5fdefa85aa
jhsung-PC\jhsung <propose34@gmail.com>
2017-11-18 17:24:44 +0900

    section07-an-introduction-to-components

---
+++ section07-an-introduction-to-components/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="container">
+		<app-header></app-header>
+		<hr>
+		<div class="row">
+			<app-servers></app-servers>
+			<app-server-details></app-server-details>
+		</div>
+		<hr>
+		<app-footer></app-footer>
+	</div>
+</template>
+
+<script>
+import Header from './components/common/Header.vue';
+import Footer from './components/common/Footer.vue';
+import Servers from './components/server/Servers.vue';
+import ServerDetail from './components/server/ServerDetail.vue';
+
+export default {
+	components: {
+		'app-header': Header,
+		'app-servers': Servers,
+		'app-server-details': ServerDetail,
+		'app-footer': Footer
+	}
+}
+</script>
---
+++ section07-an-introduction-to-components/src/components/server/Servers.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="col-xs-12 col-sm-6">
+		<ul class="list-group">
+			<app-server v-for="index in 5" :index="index"></app-server>
+		</ul>
+	</div>
+</div>
+</template>
+
+<script>
+import Server from './Server.vue';
+
+export default {
+	components: {
+		'app-server':Server
+	}
+}
+</script>
