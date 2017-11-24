57f638884d047088fc829af25067dc8b1aa84af6
jhsung-PC\jhsung <propose34@gmail.com>
2017-11-25 02:09:30 +0900

    section15-connecting-to-servers-via-http-using-vue-resource-4th-interceptor

--- section15-connecting-to-servers-via-http-using-vue-resource/src/main.js
+++ section15-connecting-to-servers-via-http-using-vue-resource/src/main.js
@@ -8,6 +8,23 @@ Vue.use(VueResource); // $http 사용하려면 필수
 // https://github.com/pagekit/vue-resource/blob/develop/docs/config.md
 Vue.http.options.root = 'https://vuejs-http-a88d5.firebaseio.com';
 
+// http interceptor
+Vue.http.interceptors.push((request, next) => {
+	console.warn('Vue.http.interceptors: {}', request);
+	if (request.method == 'POST') {
+		request.method = 'PUT'; // firebase로 put요청 시에는 uuid를 만들지 않고, 데이터 자체를 override
+	}
+
+	// intercept response
+	next(response => {
+		response.json = () => {
+			return {
+				responseBody: response.body
+			}
+		}
+	});
+});
+
 new Vue({
 	el: '#app',
 	render: h => h(App)
